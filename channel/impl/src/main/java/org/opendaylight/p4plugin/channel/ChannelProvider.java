20433acb39a843f67c4c1664c68f2e3e4c85cacc
Ding Rui <ding.rui@zte.com.cn>
2017-07-18 16:08:32 +0800

    Add SetForwardingPipelineConfig and write table entry RPC impl
       1)test simple_router demo ok;
       2)simple_router.proto.txt and simple_router.json must be
         placed under karaf/target/assemble;
       3)many problems need to be sloved;
    
    Change-Id: I53c68f556a1b3ac243c507486661802f733a172c
    Signed-off-by: Ding Rui <ding.rui@zte.com.cn>

--- channel/impl/src/main/java/org/opendaylight/p4plugin/channel/ChannelProvider.java
+++ channel/impl/src/main/java/org/opendaylight/p4plugin/channel/ChannelProvider.java
@@ -11,11 +11,12 @@ import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class ChannelProvider {
     private static final Logger LOG = LoggerFactory.getLogger(ChannelProvider.class);
     private final DataBroker dataBroker;
 
-    GrpcClient client = null;
+    ChannelImpl client = null;
 
     public ChannelProvider(final DataBroker dataBroker) {
         this.dataBroker = dataBroker;
@@ -27,8 +28,79 @@ public class ChannelProvider {
 
     public void init() {
         LOG.info("ChannelProvider Session Initiated");
-        client = new GrpcClient("localhost",50051);
-        LOG.info("ChannelProvider " + client.greet("dingrui"));
+        client = new ChannelImpl("localhost",50051);
+        client.setForwardingPipelineConfig();
+        client.writeTableEntry(new WriteTableEntryData.Builder()
+                                .setDeviceId(0)
+                                .setUpdateType(WriteTableEntryData.UpdateType.INSERT)
+                                .setTableName("ipv4_lpm")
+                                .setActionName("set_nhop")
+                                .setFieldName("ipv4.dstAddr")
+                                .setMatchType(WriteTableEntryData.MatchType.LPM)
+                                .setMatchValue("10.0.0.0")
+                                .setPrefixLen(24)
+                                .addParams("nhop_ipv4","10.0.0.10")
+                                .addParams("port", "1")
+                                .build());
+
+        client.writeTableEntry(new WriteTableEntryData.Builder()
+                                .setDeviceId(0)
+                                .setUpdateType(WriteTableEntryData.UpdateType.INSERT)
+                                .setTableName("ipv4_lpm")
+                                .setActionName("set_nhop")
+                                .setFieldName("ipv4.dstAddr")
+                                .setMatchType(WriteTableEntryData.MatchType.LPM)
+                                .setMatchValue("10.0.1.0")
+                                .setPrefixLen(24)
+                                .addParams("nhop_ipv4","10.0.1.10")
+                                .addParams("port", "2")
+                                .build());
+
+
+        client.writeTableEntry(new WriteTableEntryData.Builder()
+                                .setDeviceId(0)
+                                .setUpdateType(WriteTableEntryData.UpdateType.INSERT)
+                                .setTableName("forward")
+                                .setActionName("set_dmac")
+                                .setFieldName("routing_metadata.nhop_ipv4")
+                                .setMatchType(WriteTableEntryData.MatchType.EXACT)
+                                .setMatchValue("10.0.0.10")
+                                .addParams("dmac","00:04:00:00:00:00")
+                                .build());
+
+        client.writeTableEntry(new WriteTableEntryData.Builder()
+                                .setDeviceId(0)
+                                .setUpdateType(WriteTableEntryData.UpdateType.INSERT)
+                                .setTableName("forward")
+                                .setActionName("set_dmac")
+                                .setFieldName("routing_metadata.nhop_ipv4")
+                                .setMatchType(WriteTableEntryData.MatchType.EXACT)
+                                .setMatchValue("10.0.1.10")
+                                .addParams("dmac","00:04:00:00:00:01")
+                                .build());
+
+        client.writeTableEntry(new WriteTableEntryData.Builder()
+                                .setDeviceId(0)
+                                .setUpdateType(WriteTableEntryData.UpdateType.INSERT)
+                                .setTableName("send_frame")
+                                .setActionName("rewrite_mac")
+                                .setFieldName("standard_metadata.egress_port")
+                                .setMatchType(WriteTableEntryData.MatchType.EXACT)
+                                .setMatchValue("1")
+                                .addParams("smac","0a:0b:0c:0d:0e:0f")
+                                .build());
+
+        client.writeTableEntry(new WriteTableEntryData.Builder()
+                                .setDeviceId(0)
+                                .setUpdateType(WriteTableEntryData.UpdateType.INSERT)
+                                .setTableName("send_frame")
+                                .setActionName("rewrite_mac")
+                                .setFieldName("standard_metadata.egress_port")
+                                .setMatchType(WriteTableEntryData.MatchType.EXACT)
+                                .setMatchValue("2")
+                                .addParams("smac","01:02:03:04:05:06")
+                                .build());
+        LOG.info("Write OK");
     }
 
 

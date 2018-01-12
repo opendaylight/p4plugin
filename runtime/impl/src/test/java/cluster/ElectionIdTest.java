72f85bb2028b039c5d1faa7c064a2637ed8e9bc8
Ding Rui <ding.rui@zte.com.cn>
2018-01-12 10:16:56 +0800

    Add election id UT
    Change-Id: If33a0ceb3eeb9346af3f45b1940fb4d511340254
    Signed-off-by: Ding Rui <ding.rui@zte.com.cn>

---
+++ runtime/impl/src/test/java/cluster/ElectionIdTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2017 zte and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package cluster;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.p4plugin.runtime.impl.cluster.ElectionId;
+import org.opendaylight.p4plugin.runtime.impl.cluster.ElectionIdGenerator;
+import org.opendaylight.p4plugin.runtime.impl.cluster.ElectionIdObserver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElectionIdTest {
+
+
+    @Before
+    public void before() {}
+
+    @Test
+    public void testGetDefaultId() {
+        ElectionId electionId = new ElectionId((long)0, (long)0);
+        Assert.assertEquals(electionId.getHigh(), ElectionIdGenerator.getInstance().getElectionId().getHigh());
+        Assert.assertEquals(electionId.getLow(), ElectionIdGenerator.getInstance().getElectionId().getLow());
+    }
+
+    @Test
+    public void testObserver() {
+        List<ElectionId> list = new ArrayList<>();
+        ElectionId electionId1 = new ElectionId((long)1, (long)2);
+        ElectionId electionId2 = new ElectionId((long)3, (long)4);
+        ElectionIdObserver observer = electionId->list.add(electionId);
+        ElectionIdGenerator.getInstance().addObserver(observer);
+        ElectionIdGenerator.getInstance().setElectionId(electionId1);
+        Assert.assertTrue(list.contains(electionId1));
+        ElectionIdGenerator.getInstance().deleteObserver(observer);
+        ElectionIdGenerator.getInstance().setElectionId(electionId2);
+        Assert.assertFalse(list.contains(electionId2));
+    }
+    @After
+    public void after() {}
+
+
+}

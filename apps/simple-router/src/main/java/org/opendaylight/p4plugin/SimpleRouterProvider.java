78e1b856770190d2ec00fcc790a03e7680749d78
Ding Rui <ding.rui@zte.com.cn>
2018-09-19 09:24:17 +0800

    new implementation
    Change-Id: I3a3aa50e1ad2e98acf734761df2ee933a6096b90
    Signed-off-by: Ding Rui <ding.rui@zte.com.cn>

---
+++ apps/simple-router/src/main/java/org/opendaylight/p4plugin/SimpleRouterProvider.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2017 zte and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.p4plugin;
+
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.p4plugin.device.rev170808.P4pluginDeviceService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.p4plugin.p4runtime.rev170808.P4pluginP4runtimeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleRouterProvider {
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleRouterProvider.class);
+    private final DataBroker dataBroker;
+    private final P4pluginDeviceService deviceService;
+    private final P4pluginP4runtimeService runtimeService;
+    private final String gRPCServerIp;
+    private final Integer gRPCServerPort;
+    private final Long deviceId;
+    private final String nodeId;
+    private final String configFile;
+    private final String runtimeFile;
+    private SimpleRouterRunner simpleRouterRunner;
+
+    public SimpleRouterProvider(final DataBroker dataBroker,
+                                final P4pluginDeviceService deviceService,
+                                final P4pluginP4runtimeService runtimeService,
+                                final String gRPCServerIp,
+                                final Integer gRPCServerPort,
+                                final Long deviceId,
+                                final String nodeId,
+                                final String configFile,
+                                final String runtimeFile) {
+        this.dataBroker = dataBroker;
+        this.deviceService = deviceService;
+        this.runtimeService = runtimeService;
+        this.gRPCServerIp = gRPCServerIp;
+        this.gRPCServerPort = gRPCServerPort;
+        this.deviceId = deviceId;
+        this.nodeId = nodeId;
+        this.configFile = configFile;
+        this.runtimeFile = runtimeFile;
+    }
+
+    public void init() {
+        simpleRouterRunner = new SimpleRouterRunner(deviceService, runtimeService, gRPCServerIp,
+                                                    gRPCServerPort, deviceId, nodeId, configFile, runtimeFile);
+        simpleRouterRunner.run();
+        LOG.info("Simple Router provider init.");
+    }
+
+    public void close() {
+        simpleRouterRunner.close();
+        LOG.info("Simple router provider closed.");
+    }
+}
---
+++ p4runtime/impl/src/main/java/org/opendaylight/p4plugin/gNMIServiceProvider.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2017 zte and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.p4plugin;
+
+public class gNMIServiceProvider {
+}

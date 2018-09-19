31d354bc66dd8c6e993a258944e87d00a50fb0e6
Ding Rui <ding.rui@zte.com.cn>
2018-09-19 09:24:17 +0800

    new implementation
    Change-Id: I3a3aa50e1ad2e98acf734761df2ee933a6096b90
    Signed-off-by: Ding Rui <ding.rui@zte.com.cn>

---
+++ p4runtime/impl/src/main/java/org/opendaylight/p4plugin/DeviceManager.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright © 2017 zte and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.p4plugin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DeviceManager {
+    private static final Logger LOG = LoggerFactory.getLogger(DeviceManager.class);
+    private static DeviceManager singleton = new DeviceManager();
+    private ConcurrentHashMap<String, Device> devices = new ConcurrentHashMap<>();
+    private DeviceManager() {}
+    public static DeviceManager getInstance() {
+        return singleton;
+    }
+
+    private boolean isNodeIdExist(String nodeId) {
+        return devices.keySet().contains(nodeId);
+    }
+
+    private boolean isServerAddressExist(String ip, Integer port, Long deviceId) {
+        Optional<String> keyContainer = devices.keySet()
+                .stream()
+                .filter(k -> devices.get(k).getDeviceId().equals(deviceId)
+                          && devices.get(k).getIp().equals(ip)
+                          && devices.get(k).getPort().equals(port))
+                .findFirst();
+        return keyContainer.isPresent();
+    }
+
+    public boolean isDeviceExist(String nodeId, String ip, Integer port, Long deviceId) {
+        return isNodeIdExist(nodeId) || isServerAddressExist(ip, port, deviceId);
+    }
+
+    public Optional<Device> findDevice(String nodeId) {
+        return Optional.ofNullable(devices.get(nodeId));
+    }
+
+    public synchronized Status addDevice(String nodeId, Long deviceId, String ip, Integer port,
+                                         String p4InfoFile, String configFile) throws IOException {
+        if (isDeviceExist(nodeId, ip, port, deviceId)) {
+            return Status.DEVICE_ALREADY_EXIST;
+        }
+
+        Status status = Status.DEVICE_ADD_SUCCESS;
+        try {
+            Device.Builder builder = Device.newBuilder()
+                    .setNodeId(nodeId)
+                    .setDeviceId(deviceId)
+                    .setP4InfoFile(p4InfoFile)
+                    .setDeviceConfigFile(configFile)
+                    .setIp(ip)
+                    .setPort(port);
+            Device device = builder.build();
+            device.openStreamChannel();
+            devices.put(nodeId, device);
+            LOG.info("Device add success, nodeId = {}.", device.getNodeId());
+        } catch (IOException e) {
+            status = Status.PARSE_FILE_EXCEPTION;
+        } catch (IllegalArgumentException e) {
+            status =  Status.INVALID_ARGUMENTS;
+        }
+        return status;
+    }
+
+    public synchronized void removeDevice(String nodeId) {
+        Optional<Device> optional = findDevice(nodeId);
+        optional.ifPresent((device)->{
+            device.closeStreamChannel();
+            devices.remove(nodeId);
+            LOG.info("Device removed, nodeId = {}.", device.getNodeId());
+        });
+    }
+
+    public List<String> queryNodes() {
+        List<String> result = new ArrayList<>();
+        devices.keySet().forEach(node->{
+            Device device = devices.get(node);
+            result.add(device.toString());
+        });
+        return result;
+    }
+
+    public enum Status {
+        DEVICE_ALREADY_EXIST(0),
+        PARSE_FILE_EXCEPTION(1),
+        INVALID_ARGUMENTS(2),
+        DEVICE_ADD_SUCCESS(3);
+        private int value;
+
+        Status(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+}
---
+++ p4runtime/impl/src/main/java/org/opendaylight/p4plugin/DeviceServiceProvider.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright © 2017 zte and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.p4plugin;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.p4plugin.device.rev170808.*;
+import org.opendaylight.yangtools.yang.common.RpcResult;
+import org.opendaylight.yangtools.yang.common.RpcResultBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+
+public class DeviceServiceProvider implements P4pluginDeviceService {
+    private static final Logger LOG = LoggerFactory.getLogger(DeviceServiceProvider.class);
+    private final DataBroker dataBroker;
+    private DeviceManager deviceManager;
+    private ListeningExecutorService executorService;
+
+    public DeviceServiceProvider(final DataBroker dataBroker) {
+        this.dataBroker = dataBroker;
+    }
+
+    public void init() {
+        executorService = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(1));
+        deviceManager = DeviceManager.getInstance();
+        LOG.info("P4 plugin device service provider initiated.");
+    }
+
+    public void close() {
+        executorService.shutdown();
+        LOG.info("P4 plugin device service provider closed.");
+    }
+
+    private <T> RpcResult<T> rpcResultSuccess(T value) {
+        return RpcResultBuilder.success(value).build();
+    }
+
+    private Callable<RpcResult<AddDeviceOutput>> addDev(AddDeviceInput input) {
+        return () -> {
+            String nodeId = input.getNid();
+            String ip = input.getIp().getValue();
+            Integer port = input.getPort().getValue();
+            Long deviceId = input.getDid().longValue();
+            String runtimeFile = input.getRuntimeFile();
+            String configFile = input.getPipelineFile();
+            DeviceManager.Status status = deviceManager.addDevice(nodeId, deviceId, ip, port, runtimeFile, configFile);
+            LOG.info("Add device = {}-{}-{}:{}-{}-{} status = {}.", nodeId, deviceId, ip, port, runtimeFile, configFile, status);
+            AddDeviceOutputBuilder outputBuilder = new AddDeviceOutputBuilder();
+            AddDeviceOutput.Status outputStatus = AddDeviceOutput.Status.forValue(status.getValue());
+            outputBuilder.setStatus(outputStatus);
+            return rpcResultSuccess(outputBuilder.build());
+        };
+    }
+
+    private Callable<RpcResult<RemoveDeviceOutput>> removeDev(RemoveDeviceInput input) {
+        return () -> {
+            deviceManager.removeDevice(input.getNid());
+            LOG.info("Remove device success, nodeId = {}.", input.getNid());
+            return rpcResultSuccess(null);
+        };
+    }
+
+    private Callable<RpcResult<QueryDevicesOutput>> queryDev() {
+        return ()->{
+            QueryDevicesOutputBuilder outputBuilder = new QueryDevicesOutputBuilder();
+            outputBuilder.setNode(deviceManager.queryNodes());
+            LOG.info("Query devices success.");
+            return rpcResultSuccess(outputBuilder.build());
+        };
+    }
+
+    @Override
+    public ListenableFuture<RpcResult<AddDeviceOutput>> addDevice(AddDeviceInput input) {
+        return executorService.submit(addDev(input));
+    }
+
+    @Override
+    public ListenableFuture<RpcResult<QueryDevicesOutput>> queryDevices(QueryDevicesInput input) {
+        return executorService.submit(queryDev());
+    }
+
+    @Override
+    public ListenableFuture<RpcResult<RemoveDeviceOutput>> removeDevice(RemoveDeviceInput input) {
+        return executorService.submit(removeDev(input));
+    }
+}
